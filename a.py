afb7d217ffcd29a8c53d5257e49abed7d7a31d46
janghun lee <janghun lee>
2016-07-31 21:26:22 +0900

    test_delete

--- a.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import ast, sys
-quest = urllib.request.urlopen('https://api.lootbox.eu/pc/kr/Pangbae-3151/quick-play/allHeroes/')
-kittens = quest.read()
-str_data = kittens.decode("utf-8")
-data = ast.literal_eval(str_data)
-print(data["SoloKills"])
